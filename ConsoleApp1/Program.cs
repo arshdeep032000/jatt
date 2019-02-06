ace66d5d7b6f747c2425a1c8b882dbe08823abaf
visualstudio <visualstudio@122-PC29>
2019-02-06 09:14:29 -0500

    hlo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace c0730385
+
 {
-    class Program
+    class arshdeep
     {
         static void Main(string[] args)
         {
+            MethodA();
         }
+        public static void MethodA()
+        {
+            while (peanut())
+            {
+                Console.WriteLine("i am stuck in an infinite loop");
+            }
+        }
+        public static bool peanut()
+        {
+            return true;
+        }
+
     }
-}
+}                   
+
+  
+    
+
